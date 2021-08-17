5b3305c51a8260a6df17ce6a450d9a5b4948cfa5
OmarVargas235 <ov1356272@gmail.com>
2021-08-17 12:24:28 -0400

    Agregando configuracion basica para correr el servidor

---
+++ config/config.js
@@ -0,0 +1,9 @@
+// =====================================
+// PUERTO
+// =====================================
+process.env.PORT = process.env.PORT || 5000;
+
+// =====================================
+// Puerto del Frontend
+// =====================================
+process.env.FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
